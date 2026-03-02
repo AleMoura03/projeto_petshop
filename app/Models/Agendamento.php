5950220d2ea113a453146caee3a7853af5926bbc
AleMoura03 <ale_moura03@hotmail.com>
2026-03-01 23:14:56 -0300

    falta agora cadastrar os serviços para testar, demais coisas testas

--- app/Models/Agendamento.php
+++ app/Models/Agendamento.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Agendamento extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'pet_id',
+        'servico_id',
+        'data',
+        'hora'
+    ];
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function pet(){
+        return $this->belongsTo(Pet::class);
+    }
+    public function servico(){
+        return $this->belongsTo(Servico::class);
+    }
 }
