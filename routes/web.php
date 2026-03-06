91d6f4b201afe017546f830259cdb3f28e9d0074
AleMoura03 <ale_moura03@hotmail.com>
2026-03-06 00:43:52 -0300

    não sei oq fiz, só fiz²

--- routes/web.php
+++ routes/web.php
@@ -6,22 +6,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-// 
-Route::middleware(['auth'])->group(function (){
 
-Route::get('/agendar', [AgendamentoController::class, 'create'])->middleware([auth])->name('agendar');
+Route::middleware(['auth'])->group(function () {
 
-Route::post('/agendar', [AgendamentoController::class, 'store'])->name('agendar.store');
+    Route::get('/agendar', [AgendamentoController::class, 'create'])
+        ->name('agendar')
+        ->middleware('auth');
+
+    Route::post('/agendar', [AgendamentoController::class, 'store'])->name('agendar.store');
+
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth'])->group(function () {
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
 });
 
 require __DIR__ . '/auth.php';
